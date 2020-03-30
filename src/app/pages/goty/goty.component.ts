a7b7d6efe37765babee496cef1316b7ac5a786e7
Nico Arato <nico.arato@gmail.com>
2020-03-30 19:29:20 -0300

    Get: obtención de los datos usando el servicio.

--- src/app/pages/goty/goty.component.ts
+++ src/app/pages/goty/goty.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from '../../services/game.service';
+import { Game } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-goty',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GotyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gameService: GameService) { }
 
+  juegos: Game[] = [];
   ngOnInit() {
+
+    this.gameService.getNominados()
+        .subscribe( juegos => {
+          console.log(juegos);
+          this.juegos = juegos;
+        });
+
+
+
+
   }
 
 }
