89c0c21668f7dc4e16743446044deecf347dee04
Nico Arato <nico.arato@gmail.com>
2020-03-30 20:04:45 -0300

    Para evitar la petición en cada cambio de pantalla.

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -2,16 +2,32 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Game } from '../interfaces/interfaces';
+import { of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
+  private juegos: Game[] = [];
 
   constructor( private http: HttpClient) { }
 
   getNominados() {
-    return this.http.get< Game[] >( `${ environment.url }/api/goty` );
+
+    if ( this.juegos.length > 0 ) {
+      console.log('Juegos desde cache.');
+      return of(this.juegos); //of devuelve cualquier cosa como un Observable
+    } else {
+      console.log('Juegos desde internet.');
+      return this.http.get< Game[] >( `${ environment.url }/api/goty` )
+      .pipe(
+        tap(
+          juegos => this.juegos = juegos
+        )
+      );
+
+    }
   }
 }
